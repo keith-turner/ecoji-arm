7e50cbdcfd60008a86134146e4f7745d433bd420
Keith Turner <kturner@apache.org>
2023-04-30 04:42:33 +0000

    update arm asm

--- ecoji.c
+++ ecoji.c
@@ -142,23 +142,30 @@ int ecoji_decode(FILE *infp, FILE *outfp) {
 
 #ifdef ARM_ASM
     uint64_t bits;
-    __asm("UBFIZ %[output],%[input_d1],30,10\n\t"
-          "BFI %[output],%[input_d2],20,10\n\t"
-          "BFI %[output],%[input_d3],10,10\n\t"
-          "BFI %[output],%[input_d4],0,10"
-          : [output] "=r"(bits)
-          : [input_d1] "r"(d1), [input_d2] "r"(d2), [input_d3] "r"(d3),
-            [input_d4] "r"(d4));
+    unsigned char data[8];
+    __asm("UBFIZ %[bits],%[d1],30,10\n\t"
+          "BFI %[bits],%[d2],20,10\n\t"
+          "BFI %[bits],%[d3],10,10\n\t"
+          "BFI %[bits],%[d4],0,10\n\t"
+          "REV %[bits],%[bits]\n\t"
+          "STR %[bits],%[data]"
+          : [bits] "=r"(bits), [data] "=m"(data)
+          : [d1] "r"(d1), [d2] "r"(d2), [d3] "r"(d3), [d4] "r"(d4));
+
+    unsigned char *output = data + 3;
+
 #else
     uint64_t bits = (0x3ff & (uint64_t)d1) << 30 |
                     (0x3ff & (uint64_t)d2) << 20 |
                     (0x3ff & (uint64_t)d3) << 10 | 0x3ff & (uint64_t)d4;
-#endif
 
     unsigned char output[5];
     output[0] = 0xff & (bits >> 32);
     *((uint32_t *)(&output[1])) = ntohl(bits);
-    if (fwrite_unlocked(output, 1, len, outfp) != 5) {
+
+#endif
+
+    if (fwrite_unlocked(output, 1, len, outfp) != len) {
       return -1;
     }
   }
